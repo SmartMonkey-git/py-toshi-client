3f549cf27cd107ebe0995b24bf9de03626e27795
smart_monkey <rouven@propellerheads.xyz>
2024-07-01 14:20:45 +0200

    AsyncToshiClient: Add first class

--- py-toshi-client/tests/test_integration_async.py
+++ py-toshi-client/tests/test_integration_async.py
@@ -1,15 +1,13 @@
-import time
+import asyncio
+import json
 from pathlib import Path
 
+import aiohttp
 import pytest
-import requests
 
-from client import ToshiClient
+from client import AsyncToshiClient
 from errors import ToshiIndexError
 from index import IndexSummary, IndexSettings
-from index.field_options import TextOptionIndexing
-from index.index_builder import IndexBuilder
-from models.document import Document
 from query import TermQuery
 from query.bool_query import BoolQuery
 from query.facet_query import FacetQuery
@@ -17,131 +15,41 @@ from query.fuzzy_query import FuzzyQuery
 from query.phrase_query import PhraseQuery
 from query.range_query import RangeQuery
 from query.regex_query import RegexQuery
-from tests.conftest import CI
-
-
-class Lyrics(Document):
-    @staticmethod
-    def index_name() -> str:
-        return "lyrics"
-
-    def __init__(
-        self,
-        lyrics: str,
-        year: int,
-        idx: int,
-        artist: str,
-        genre: str,
-        song: str,
-        test_facet: str,
-    ):
-        self.lyrics = lyrics
-        self.year = year
-        self.idx = idx
-        self.artist = artist
-        self.genre = genre
-        self.song = song
-        self.test_facet = test_facet
-
-
-@pytest.fixture
-def black_keys_lyrics_document():
-    return Lyrics(
-        lyrics="Gold on the ceiling, I ain't blind, just a matter of time",
-        year=2011,
-        idx=2,
-        artist="The Black Keys",
-        genre="Rock",
-        song="Gold on the Ceiling",
-        test_facet="/a/b",
-    )
-
-
-@pytest.fixture
-def nirvana_lyrics_document():
-    return Lyrics(
-        lyrics="With the lights out, it's less dangerous, here we are now, entertain us",
-        year=1991,
-        idx=4,
-        artist="Nirvana",
-        genre="Grunge",
-        song="Smells Like Teen Spirit",
-        test_facet="/a/b",
-    )
-
-
-@pytest.fixture
-def radiohead_lyrics_document():
-    return Lyrics(
-        lyrics="I'm a creep, I'm a weirdo, what the hell am I doing here?",
-        year=1992,
-        idx=3,
-        artist="Radiohead",
-        genre="Alternative Rock",
-        song="Creep",
-        test_facet="/a/b",
-    )
-
-
-@pytest.fixture
-def lyric_documents(
-    black_keys_lyrics_document, nirvana_lyrics_document, radiohead_lyrics_document
-):
-    return [
-        black_keys_lyrics_document,
-        nirvana_lyrics_document,
-        radiohead_lyrics_document,
-    ]
-
-
-@pytest.fixture
-def lyrics_index():
-    builder = IndexBuilder()
-
-    builder.add_text_field(name="lyrics", stored=True, indexing=TextOptionIndexing())
-    builder.add_i64_field(name="year", stored=True, indexed=True)
-    builder.add_u64_field(name="idx", stored=True, indexed=True)
-    builder.add_text_field(name="artist", stored=True, indexing=TextOptionIndexing())
-    builder.add_text_field(name="genre", stored=True, indexing=TextOptionIndexing())
-    builder.add_text_field(name="song", stored=True, indexing=TextOptionIndexing())
-    builder.add_facet_field(name="test_facet", stored=True)
-
-    return builder.build("lyrics")
+from tests.conftest import CI, Lyrics
 
 
 @pytest.mark.integration()
 @pytest.mark.skipif(CI, reason="Integration Test")
-def test_create_index(lyrics_index, toshi_container):
+@pytest.mark.asyncio
+async def test_create_index(lyrics_index, toshi_container):
     unknown_index_response = {"message": "Unknown Index: 'lyrics' does not exist"}
 
     get_schema_summary_url = (
         f"{toshi_container}/{lyrics_index.name}/_summary?include_sizes=true"
     )
-    res = requests.get(
-        get_schema_summary_url, headers={"Content-Type": "application/json"}
-    )
-    assert res.json() == unknown_index_response
+    async with aiohttp.ClientSession() as session:
+        async with session.get(get_schema_summary_url) as res:
+            assert json.loads(await res.read()) == unknown_index_response
 
-    client = ToshiClient(toshi_container)
-    client.create_index(index=lyrics_index)
+    client = AsyncToshiClient(toshi_container)
+    await client.create_index(index=lyrics_index)
 
-    get_schema_summary_url = (
-        f"{toshi_container}/{lyrics_index.name}/_summary?include_sizes=true"
-    )
-    res = requests.get(
-        get_schema_summary_url, headers={"Content-Type": "application/json"}
-    )
-    assert res.json() != unknown_index_response
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+            get_schema_summary_url, headers={"Content-Type": "application/json"}
+        ) as res:
+            assert await res.json() != unknown_index_response
 
     with pytest.raises(ToshiIndexError):
-        client.create_index(index=lyrics_index)
+        await client.create_index(index=lyrics_index)
 
 
 @pytest.mark.integration()
 @pytest.mark.skipif(CI, reason="Integration Test")
-def test_get_index_summary(toshi_container, lyrics_index):
-    client = ToshiClient(toshi_container)
-    res = client.get_index_summary(name=lyrics_index.name)
+@pytest.mark.asyncio
+async def test_get_index_summary(toshi_container, lyrics_index):
+    client = AsyncToshiClient(toshi_container)
+    res = await client.get_index_summary(name=lyrics_index.name)
     expected_summary = IndexSummary(
         segments=[],
         opstamp=0,
@@ -156,12 +64,13 @@ def test_get_index_summary(toshi_container, lyrics_index):
 
 @pytest.mark.integration()
 @pytest.mark.skipif(CI, reason="Integration Test")
-def test_add_document(toshi_container, lyric_documents):
-    client = ToshiClient(toshi_container)
+@pytest.mark.asyncio
+async def test_add_document(toshi_container, lyric_documents):
+    client = AsyncToshiClient(toshi_container)
     for doc in lyric_documents:
-        client.add_document(document=doc)
-    time.sleep(0.5)
-    retrieved_doc = client.get_documents(document=Lyrics)
+        await client.add_document(document=doc)
+    await asyncio.sleep(0.5)
+    retrieved_doc = await client.get_documents(document=Lyrics)
 
     assert len(retrieved_doc) == 3
     assert sorted(
@@ -176,28 +85,30 @@ def test_add_document(toshi_container, lyric_documents):
 
 @pytest.mark.integration()
 @pytest.mark.skipif(CI, reason="Integration Test")
-def test_list_indexes(toshi_container):
-    client = ToshiClient(toshi_container)
-    res = client.list_indexes()
+@pytest.mark.asyncio
+async def test_list_indexes(toshi_container):
+    client = AsyncToshiClient(toshi_container)
+    res = await client.list_indexes()
 
     assert res == ["lyrics"]
 
 
 @pytest.mark.integration()
 @pytest.mark.skipif(CI, reason="Integration Test")
-def test_search_term_query(
+@pytest.mark.asyncio
+async def test_search_term_query(
     toshi_container, black_keys_lyrics_document, lyric_documents
 ):
-    client = ToshiClient(toshi_container)
+    client = AsyncToshiClient(toshi_container)
 
     query = TermQuery(term="ceiling", field_name="lyrics")
-    documents = client.search(query, Lyrics)
+    documents = await client.search(query, Lyrics)
 
     assert len(documents) == 1
     assert documents[0] == black_keys_lyrics_document
 
     query = TermQuery(term="the", field_name="lyrics")
-    documents = client.search(query, Lyrics)
+    documents = await client.search(query, Lyrics)
 
     assert len(documents) == 3
     assert [d for d in documents] == [d for d in lyric_documents]
@@ -205,13 +116,14 @@ def test_search_term_query(
 
 @pytest.mark.integration()
 @pytest.mark.skipif(CI, reason="Integration Test")
-def test_search_fuzzy_query(toshi_container, black_keys_lyrics_document):
-    client = ToshiClient(toshi_container)
+@pytest.mark.asyncio
+async def test_search_fuzzy_query(toshi_container, black_keys_lyrics_document):
+    client = AsyncToshiClient(toshi_container)
     query = FuzzyQuery(
         term="ceilin", field_name="lyrics", distance=2, transposition=False
     )
 
-    documents = client.search(query, Lyrics)
+    documents = await client.search(query, Lyrics)
 
     assert len(documents) == 1
     assert documents[0] == black_keys_lyrics_document
@@ -219,21 +131,23 @@ def test_search_fuzzy_query(toshi_container, black_keys_lyrics_document):
 
 @pytest.mark.integration()
 @pytest.mark.skipif(CI, reason="Integration Test")
-def test_search_range_query(toshi_container):
-    client = ToshiClient(toshi_container)
+@pytest.mark.asyncio
+async def test_search_range_query(toshi_container):
+    client = AsyncToshiClient(toshi_container)
     gt = 1990
     lt = 2000
     query = RangeQuery(gt=gt, lt=lt, gte=gt, lte=lt, field_name="year")
 
-    documents = client.search(query, Lyrics)
+    documents = await client.search(query, Lyrics)
     assert len(documents) >= 1
     all(gt < doc.year < lt for doc in documents)
 
 
 @pytest.mark.integration()
 @pytest.mark.skipif(CI, reason="Integration Test")
-def test_search_bool_query(toshi_container, black_keys_lyrics_document):
-    client = ToshiClient(toshi_container)
+@pytest.mark.asyncio
+async def test_search_bool_query(toshi_container, black_keys_lyrics_document):
+    client = AsyncToshiClient(toshi_container)
 
     term_query = TermQuery(term="the", field_name="lyrics")
     gt = 1990
@@ -241,19 +155,20 @@ def test_search_bool_query(toshi_container, black_keys_lyrics_document):
     range_query = RangeQuery(gt=gt, lt=lt, field_name="year")
     query = BoolQuery().must_match(term_query).must_not_match(range_query)
 
-    documents = client.search(query, Lyrics)
+    documents = await client.search(query, Lyrics)
     assert len(documents) >= 1
     assert documents[0] == black_keys_lyrics_document
 
 
 @pytest.mark.integration()
 @pytest.mark.skipif(CI, reason="Integration Test")
-def test_regex_query(toshi_container, lyric_documents):
+@pytest.mark.asyncio
+async def test_regex_query(toshi_container, lyric_documents):
     regex = ".*"
-    client = ToshiClient(toshi_container)
+    client = AsyncToshiClient(toshi_container)
 
     query = RegexQuery(regex=regex, field_name="lyrics")
-    documents = client.search(query, Lyrics)
+    documents = await client.search(query, Lyrics)
 
     assert [d for d in sorted(documents, key=lambda doc: doc.artist)] == [
         d for d in sorted(lyric_documents, key=lambda doc: doc.artist)
@@ -262,18 +177,20 @@ def test_regex_query(toshi_container, lyric_documents):
 
 @pytest.mark.integration()
 @pytest.mark.skipif(CI, reason="Integration Test")
-def test_phrase_query(toshi_container, radiohead_lyrics_document):
-    client = ToshiClient(toshi_container)
+@pytest.mark.asyncio
+async def test_phrase_query(toshi_container, radiohead_lyrics_document):
+    client = AsyncToshiClient(toshi_container)
 
     query = PhraseQuery(terms=["what", "the", "hell"], field_name="lyrics")
-    documents = client.search(query, Lyrics)
+    documents = await client.search(query, Lyrics)
 
     assert documents[0] == radiohead_lyrics_document
 
 
 @pytest.mark.integration()
 @pytest.mark.skipif(CI, reason="Integration Test")
-def test_bulk_insert_documents(toshi_container):
+@pytest.mark.asyncio
+async def test_bulk_insert_documents(toshi_container):
     lyric_documents = [
         Lyrics(
             lyrics="There's a lady who's sure all that glitters is gold, and she's buying a stairway to heaven",
@@ -304,10 +221,10 @@ def test_bulk_insert_documents(toshi_container):
         ),
     ]
 
-    client = ToshiClient(toshi_container)
-    client.bulk_insert_documents(documents=lyric_documents, commit=True)
-    time.sleep(0.5)
-    retrieved_doc = client.get_documents(document=Lyrics)
+    client = AsyncToshiClient(toshi_container)
+    await client.bulk_insert_documents(documents=lyric_documents, commit=True)
+    await asyncio.sleep(0.5)
+    retrieved_doc = await client.get_documents(document=Lyrics)
 
     assert len(retrieved_doc) == 6
     assert all(doc in retrieved_doc for doc in lyric_documents)
@@ -315,33 +232,37 @@ def test_bulk_insert_documents(toshi_container):
 
 @pytest.mark.integration()
 @pytest.mark.skipif(CI, reason="Integration Test")
-def test_search_facet_query(
+@pytest.mark.asyncio
+async def test_search_facet_query(
     toshi_container, black_keys_lyrics_document, lyric_documents
 ):
-    client = ToshiClient(toshi_container)
+    client = AsyncToshiClient(toshi_container)
 
     query = TermQuery(term="the", field_name="lyrics")
     facet_queries = [FacetQuery("test_facet", [Path("/t")])]
-    documents = client.search(query, Lyrics, facet_query=facet_queries)
+    documents = await client.search(query, Lyrics, facet_query=facet_queries)
 
     assert len(documents) == 4
 
 
 @pytest.mark.integration()
 @pytest.mark.skipif(CI, reason="Integration Test")
-def test_delete_index(toshi_container):
-    client = ToshiClient(toshi_container)
+@pytest.mark.asyncio
+async def test_delete_index(toshi_container):
+    client = AsyncToshiClient(toshi_container)
     term_queries = [
         TermQuery(term="the", field_name="lyrics"),
         TermQuery(term="Nirvana", field_name="artist"),
     ]
 
     get_term_query = TermQuery(term="the", field_name="lyrics")
-    documents = client.search(get_term_query, Lyrics)
+    documents = await client.search(get_term_query, Lyrics)
     assert len(documents) == 4
 
-    client.delete_term(term_queries=term_queries, index_name="lyrics", commit=True)
-    time.sleep(0.5)
+    await client.delete_term(
+        term_queries=term_queries, index_name="lyrics", commit=True
+    )
+    await asyncio.sleep(0.5)
 
-    documents = client.search(get_term_query, Lyrics)
+    documents = await client.search(get_term_query, Lyrics)
     assert len(documents) == 0
